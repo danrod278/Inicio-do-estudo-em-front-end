75eaca832633e75e22cc6a9e8bca3348c84c9a2e
Daniel Rodrigues <danrod278@gmail.com>
2025-01-31 19:08:15 -0300

    correção erro de escrita

--- src/Components/Rotas/Home.js
+++ src/Components/Rotas/Home.js
@@ -9,7 +9,7 @@ function Home(){
         <>
             <div className={styles.divMain}>
                 <h2>Bem vindo ao <span className={styles.span}>Cost</span></h2>
-                <p>Faça seus principais projetos virarem realidade com o Coast</p>
+                <p>Faça seus principais projetos virarem realidade com o Cost</p>
                 <div onClick={Redirecionar}><div>Criar Projeto</div></div>
                 <img src="https://getillustrations.b-cdn.net//packs/misty-line-illustrations/scenes/_1x/security%20_%20safety,%20protection,%20safe,%20vault,%20cracking,%20locked%20out,%20woman,%20people,%20person_demo.png"/>
             </div>
