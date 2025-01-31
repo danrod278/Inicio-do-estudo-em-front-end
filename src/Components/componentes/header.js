12604a2021b03f2f914245850eae6cb59c22f9ba
Daniel Rodrigues <danrod278@gmail.com>
2025-01-31 19:04:36 -0300

    primeira pagina responsiva (home)

--- src/Components/componentes/header.js
+++ src/Components/componentes/header.js
@@ -1,6 +1,14 @@
 import style from '../styles/header.module.css'
 import {TbCoinFilled } from 'react-icons/tb'
+import { FiMenu } from "react-icons/fi";
+import { useState } from 'react';
+import { IoCloseCircleOutline } from "react-icons/io5";
+
+
 function Header(){
+
+    const [menuState, setMenuState] = useState(false)
+
     return(
         <header className={style.header}>
             <div>
@@ -16,6 +24,19 @@ function Header(){
                         <li><a className={style.link} href="/empresa">Empresa</a></li>
                     </ul>
                 </nav>
+               
+                <FiMenu className={style.menu} onClick={()=>setMenuState(!menuState)} />
+                <div className={`${style.linksMenu} ${menuState ? style.menuOn : ""}`}>
+                    <ul>
+                        <li><IoCloseCircleOutline className={style.fechar} onClick={()=>setMenuState(false)} />
+                        </li>
+                        <li><a className={style.linkMenu} href="/">Home</a></li>
+                        <li><a className={style.linkMenu} href="/projetos">Projetos</a></li>
+                        <li><a className={style.linkMenu} href="/empresa">Empresa</a></li>
+                        <li><a className={style.linkMenu} href="/contato">Contato</a></li>
+                    </ul>
+                        
+                </div>
             </div>
         </header>
     )
